7d734c4354952d26731a888e771f72eeea27d0cb
Ralph Tandetzky <ralph.tandetzky@googlemail.com>
2022-11-18 00:29:37 +0100

    Documented `ops_with_ref.rs` traits.

--- src/algebra/ops_with_ref.rs
+++ src/algebra/ops_with_ref.rs
@@ -1,6 +1,12 @@
 use std::ops::{AddAssign, Mul, Neg, SubAssign};
 
+/// This trait allows zero overhead implementation of the `+=` operator by
+/// taking operands by reference.
+///
+/// See the documentation of [`Ring`](crate::algebra::Ring) for a detailed
+/// explanation on the design rationale.
 pub trait AddAssignWithRef {
+    /// Adds `*rhs` to `self` in-place.
     fn add_assign_with_ref(&mut self, rhs: &Self);
 }
 
@@ -10,7 +16,13 @@ impl<T: AddAssign<T> + Copy> AddAssignWithRef for T {
     }
 }
 
+/// This trait allows zero overhead implementation of the `-=` operator by
+/// taking operands by reference.
+///
+/// See the documentation of [`Ring`](crate::algebra::Ring) for a detailed
+/// explanation on the design rationale.
 pub trait SubAssignWithRef {
+    /// Subtracts `*rhs` from `self` in-place.
     fn sub_assign_with_ref(&mut self, rhs: &Self);
 }
 
@@ -23,7 +35,13 @@ where
     }
 }
 
+/// This trait allows zero overhead implementation of the `+=` operator by
+/// taking operands by reference.
+///
+/// See the documentation of [`Ring`](crate::algebra::Ring) for a detailed
+/// explanation on the design rationale.
 pub trait MulWithRef {
+    /// Multiplies `*self` and `*rhs`.
     fn mul_with_ref(&self, rhs: &Self) -> Self;
 }
 
@@ -33,7 +51,13 @@ impl<T: Mul<T, Output = Self> + Copy> MulWithRef for T {
     }
 }
 
+/// This trait allows zero overhead implementation of the unary `-` operator
+/// by taking its operand by mutable reference.
+///
+/// See the documentation of [`Ring`](crate::algebra::Ring) for a detailed
+/// explanation on the design rationale.
 pub trait NegAssign {
+    /// Applies the unary `-` operator in-place.
     fn neg_assign(&mut self);
 }
 
