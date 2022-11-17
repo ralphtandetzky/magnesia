ccd3204eb1d95eec2c61405f1a500c7b8894a2a8
Ralph Tandetzky <ralph.tandetzky@googlemail.com>
2022-11-18 00:29:37 +0100

    Added crate level documentation in lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,25 @@
+//! A math library for Rust featuring
+//! * Polynomials
+//!
+//! # Goal and Scope
+//!
+//! The goal of Magnesia is to provide an easy to use yet efficient API for
+//! mathematical applications.
+//! Magnesia may not be feature rich and optimized yet, but its interfaces are
+//! designed such that extensions and zero overhead implementations are
+//! possible.
+//!
+//! In the future the package shall contain the following features as well:
+//! * Vectors and Matrices
+//! * Complex numbers and Quaternions
+//! * Fast Fourier Transform
+//! * Symbolic and numerical differentiation
+//! * Numerical Optimization
+//! * Root finding
+//! * Interpolation
+//! * Integration and solving differential equations
+//! * Geometrical primitives
+//! * Etc.
+//! Contributions for this cause are highly welcome!
+
 pub mod algebra;
