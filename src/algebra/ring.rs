8090eafba6a542de98c6153246e53da2b648bcf0
Ralph Tandetzky <ralph.tandetzky@googlemail.com>
2022-11-18 00:29:37 +0100

    Updated the documentation of `Ring`.

--- src/algebra/ring.rs
+++ src/algebra/ring.rs
@@ -29,17 +29,21 @@ impl<T: From<i8>> One for T {
 /// Trait for data structures which model an
 /// [algebraic ring](https://en.wikipedia.org/wiki/Ring_(mathematics)).
 ///
+/// # Implementing and Using the `Ring` Trait
+///
 /// In short, a ring in mathematics is an algebraic structure that is
-/// * an abelian group under addition,
-/// * a monoid under multiplication,
-/// * with multiplication being distributive with respect to addition.
-/// Please see https://en.wikipedia.org/wiki/Ring_(mathematics) for more
-/// details.
+///
+///   * an abelian group under addition,
+///   * a monoid under multiplication,
+///   * with multiplication being distributive with respect to addition.
+///
+/// Please read [this Wikipedia page on rings](https://en.wikipedia.org/wiki/Ring_(mathematics))
+/// for more details.
 /// Hence, when implementing the ring trait, the respective mathematical laws
 /// (like the commutative law, etc.) should be guaranteed, so the code which
 /// uses the `Ring` trait will work as expected.
-/// This being said does not mean that computations always need to be exact.
-/// For example, IEEE 754 floating point types like `f32` or `f64` only have
+/// This being said it does not mean that computations always need to be exact.
+/// For example, IEEE-754 floating point types like `f32` or `f64` only have
 /// limited precision and suffer from rounding.
 /// Therefore mathematical laws are compromised.
 /// For example, given three floating point values `x`, `y` and `z`, the
@@ -49,6 +53,8 @@ impl<T: From<i8>> One for T {
 /// This uncertainty should be dealt with properly by code which uses the
 /// `Ring` trait.
 ///
+/// # Design Rationale of the Ring Trait
+///
 /// The `Ring` trait requires a number of standard traits to be implemented
 /// which allow easy handling and operator overloading.
 /// In addition, there are some non-standard traits.
@@ -57,25 +63,33 @@ impl<T: From<i8>> One for T {
 /// The traits `AddAssignWithRef`, `SubAssignWithRef` and `MulWithRef` may
 /// appear a bit strange at first.
 /// However, they are important to implement some structures with zero
-/// overhead.
-/// For example, one may implement a ring of arbitrarily large integers which
-/// may be expensive to copy.
-/// The normal addition and multiplication operators would always *move* both
-/// operands into the called operator function, even though a reference on
-/// both sides would be completely sufficient.
-/// This would force the user of the large integer data structure to clone
-/// borrowed instances unnecessarily.
-/// This becomes even more problematic, if you want to deal with polynomials
-/// of such integers.
-/// These polynomials would always have to clone and move coefficients and
-/// values which can cause a lot of overhead.
-/// To allow efficient implementations for types that may be expensive to
-/// clone these traits are required.
-/// Fortunately, for copyable types for which the standard operator traits are
-/// available, the traits will be implemented automatically.
+/// overhead as we'll explain now.
 ///
-/// To fully support rings with floating point number structures, we only
-/// require `PartialEq`, not `Eq` to be implemented.
+/// Arithmetic operator traits from the standard library usually take at least
+/// one of their arguments by value.
+/// If the data type for which the operator is implemented is expensive to
+/// clone, then it may be preferable to pass operands by reference.
+/// Otherwise, clients may be forced to clone borrowed instances
+/// unnecessarily.
+/// Unfortunately, it is impossible to implement an `Add` operator for `&i32`
+/// for example.
+/// Hence, in type-generic code, requiring an `Add` implementation for `&T`
+/// precludes built-in types like `i32` to be used.
+/// But it should be possible to implement abstract mathematical structures
+/// (like polynomials or matrices) for both built-in element types and element
+/// types which are potentially expensive to copy (like large integers)
+/// efficiently.
+/// This is what the operator traits [`AddAssignWithRef`], [`SubAssignWithRef`]
+/// and [`MulWithRef`] are designed for.
+///
+/// You may notice that we provide in-place operators for addition and
+/// subtraction ([`AddAssignWithRef`] and [`SubAssignWithRef`]), but the
+/// multiplication operator [`MulWithRef`] is not in-place, but returns `Self`.
+/// This distinction is made on purpose, because for most data structures
+/// addition and subtraction can be performed in-place very efficiently,
+/// but multiplication cannot be performed in-place efficiently.
+/// (Please convince yourself by looking considering matrices or polynomials
+/// as examples.)
 pub trait Ring:
     Clone
     + Zero
