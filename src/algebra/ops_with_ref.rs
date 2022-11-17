79b3a485f48d18b73c3c6413349403ac64b6ca49
Ralph Tandetzky <ralph.tandetzky@googlemail.com>
2022-11-18 00:29:37 +0100

    Simplified custom operator traits.
    Now they require less. The file is also less verbose due to `use` at the top.

--- src/algebra/ops_with_ref.rs
+++ src/algebra/ops_with_ref.rs
@@ -1,19 +1,20 @@
+use std::ops::{AddAssign, Mul, Neg, SubAssign};
+
 pub trait AddAssignWithRef {
     fn add_assign_with_ref(&mut self, rhs: &Self);
 }
 
-impl<T: std::ops::AddAssign<T> + Copy> AddAssignWithRef for T
-{
+impl<T: AddAssign<T> + Copy> AddAssignWithRef for T {
     fn add_assign_with_ref(&mut self, rhs: &Self) {
         *self += *rhs;
     }
 }
 
-pub trait SubAssignWithRef: Clone + std::ops::Sub<Self, Output = Self> + std::ops::SubAssign<Self> {
+pub trait SubAssignWithRef {
     fn sub_assign_with_ref(&mut self, rhs: &Self);
 }
 
-impl<T: std::ops::Sub<T, Output = Self> + std::ops::SubAssign<T> + Clone> SubAssignWithRef for T
+impl<T: SubAssign<T> + Clone> SubAssignWithRef for T
 where
     T: Copy,
 {
@@ -22,24 +23,21 @@ where
     }
 }
 
-pub trait MulWithRef
-{
-    fn mul_with_ref(&self, rhs :&Self) -> Self;
+pub trait MulWithRef {
+    fn mul_with_ref(&self, rhs: &Self) -> Self;
 }
 
-impl<T: std::ops::Mul<T, Output = Self> + Copy> MulWithRef for T
-{
-    fn mul_with_ref(&self, rhs :&Self) -> Self
-    {
+impl<T: Mul<T, Output = Self> + Copy> MulWithRef for T {
+    fn mul_with_ref(&self, rhs: &Self) -> Self {
         *self * *rhs
     }
 }
 
-pub trait NegAssign: Clone + std::ops::Neg<Output = Self> {
+pub trait NegAssign {
     fn neg_assign(&mut self);
 }
 
-impl<T: std::ops::Neg<Output = T> + Clone> NegAssign for T
+impl<T: Neg<Output = T> + Clone> NegAssign for T
 where
     T: Copy,
 {
